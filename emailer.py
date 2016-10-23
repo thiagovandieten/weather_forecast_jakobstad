ad1c31c8546f7f9412ae3f1643ce9ff7b75950da
Thiago van Dieten <nktakumi@gmail.com>
2016-10-23 23:14:01 +0300

    add send_emails method

--- emailer.py
+++ emailer.py
@@ -1,4 +1,5 @@
 import requests
+import smtplib
 
 emails = {}
 
@@ -29,8 +30,7 @@ def get_schedule():
         print(err)
 
 def get_weather_forecast():
-    #City ID: 656130
-    #API key: 9f57233f8f1a7130f3952bc02a84139e
+    #Depends on the requests module
     url = 'http://api.openweathermap.org/data/2.5/weather?id=656130&units=metric&APPID=9f57233f8f1a7130f3952bc02a84139e'
     r = requests.get(url)
     weather_json = r.json()
@@ -38,20 +38,51 @@ def get_weather_forecast():
     description = weather_json['weather'][0]['description']
     temp_min = weather_json['main']['temp_min']
     temp_max = weather_json['main']['temp_max']
+    city = weather_json['name']
 
-    forecast = 'The circus of Jakobstad\'s forecast for today is '
+    forecast = 'The circus of ' + city + '\'s forecast for today is '
     forecast += description + ' with a high of ' + str(int(temp_max))
-    forecast += 'C\u00b0 and a low of ' + str(int(temp_min)) + 'C\u00b0'
+    forecast += ' celsius and a low of ' + str(int(temp_min)) + ' celsius'
 
-    print(forecast)
+    return forecast
+
+def send_emails(emails, schedule, forecast):
+    try:
+        # Get the emailer's user & pass
+        # account_file = open('mailaccount.txt', 'r')
+        # (mail_user, password) = account_file.readline().split(',')
+        # account_file.close()
+
+        #Changed to using input from the user itself
+        mail_user = input('Please login into the mail server with your email: ')
+        mail_user = mail_user.strip()
+        password = input('And your password: ').strip()
+
+        # Connect to the SMTP server
+        server = smtplib.SMTP('smtp.gmail.com', '587')
+
+        #Start TLS encryption
+        server.starttls()
+        server.login(mail_user, password)
+        server.set_debuglevel(1)
+
+        for address, name in emails.items():
+            server.sendmail(mail_user, address, forecast)
+
+        server.quit()
+
+    except Exception as err:
+        print(err)
 
 def main():
-    # emails = get_emails()
+    emails = get_emails()
     # print(emails)
-    #
-    # schedule = get_schedule()
+
+    schedule = get_schedule()
     # print(schedule)
 
-    get_weather_forecast()
+    forecast = get_weather_forecast()
+
+    send_emails(emails, schedule, forecast)
 
 main()
