c36a3521cc5a560b3d03dbf3395ba53cc5faa055
Thiago van Dieten <nktakumi@gmail.com>
2016-10-23 07:59:05 +0300

    read schedule & pack everything in their own function

--- emailer.py
+++ emailer.py
@@ -1,14 +1,36 @@
 emails = {}
-try:
-    email_file = open('emails.txt', 'r')
 
-    for line in email_file:
-        line = line.strip()
-        (email, name) = line.split(',')
-        emails[email] = name
+def get_emails():
 
-    email_file.close()
+    try:
+        email_file = open('emails.txt', 'r') #CSV, starting with email.
+
+        for line in email_file:
+            line = line.strip()
+            (email, name) = line.split(',')
+            emails[email] = name
+
+        email_file.close()
+        return emails
+
+    except FileNotFoundError as err:
+        print(err)
+
+def get_schedule():
+    try:
+        schedule_file = open('schedule.txt', 'r')
+        schedule = schedule_file.read()
+        schedule_file.close()
+        return schedule
+
+    except FileNotFoundError as err:
+        print(err)
+
+def main():
+    emails = get_emails()
     print(emails)
 
-except FileNotFoundError as err:
-    print(err)
+    schedule = get_schedule()
+    print(schedule)
+
+main()
