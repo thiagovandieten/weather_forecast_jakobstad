9bbd9d8e0a1933090546b32cb4d46f35a5c03d3e
Thiago van Dieten <nktakumi@gmail.com>
2016-10-23 08:50:46 +0300

    add get_weather_forecast

--- emailer.py
+++ emailer.py
@@ -1,3 +1,5 @@
+import requests
+
 emails = {}
 
 def get_emails():
@@ -26,11 +28,30 @@ def get_schedule():
     except FileNotFoundError as err:
         print(err)
 
-def main():
-    emails = get_emails()
-    print(emails)
+def get_weather_forecast():
+    #City ID: 656130
+    #API key: 9f57233f8f1a7130f3952bc02a84139e
+    url = 'http://api.openweathermap.org/data/2.5/weather?id=656130&units=metric&APPID=9f57233f8f1a7130f3952bc02a84139e'
+    r = requests.get(url)
+    weather_json = r.json()
 
-    schedule = get_schedule()
-    print(schedule)
+    description = weather_json['weather'][0]['description']
+    temp_min = weather_json['main']['temp_min']
+    temp_max = weather_json['main']['temp_max']
+
+    forecast = 'The circus of Jakobstad\'s forecast for today is '
+    forecast += description + ' with a high of ' + str(int(temp_max))
+    forecast += 'C\u00b0 and a low of ' + str(int(temp_min)) + 'C\u00b0'
+
+    print(forecast)
+
+def main():
+    # emails = get_emails()
+    # print(emails)
+    #
+    # schedule = get_schedule()
+    # print(schedule)
+
+    get_weather_forecast()
 
 main()
